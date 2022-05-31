e591f95fd9155c2a9084deff741a6bc4a88763cd
Osama Sultan <osamasultan189@gmail.com>
2022-05-31 10:04:29 +0500

    Dua App Main Activity Added

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button learn,quiz,git;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        learn=findViewById(R.id.learn);
+        quiz=findViewById(R.id.quiz);
+        git=findViewById(R.id.git);
+        learn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,learnActivity.class);
+                startActivity(intent);
+            }
+        });
+        quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,quizActivity.class);
+                startActivity(intent);
+            }
+        });
+        git.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gitPage=Uri.parse("https://github.com/Osama-Sultan189/Mobile-Computing");
+                Intent intent=new Intent(Intent.ACTION_VIEW,gitPage);
+            }
+        });
     }
 }
